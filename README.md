66df1f53355ff5eb294de83ee5d43fef032bc01d
kristof.vorosmarty <kristof.vorosmarty@weborbit.hu>
2022-08-10 09:48:06 +0200

    Added generator and wrote README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# drupal9entitygenerator
+# futtatás: php generate.php data.txt
+#
+# a data.txt tartalma a következőképpen nézzen ki:
+#
+# XYZ - ez az entity neve
+# id:integer - a property neve kettőspont és a típusa
+#
+# jó szórakozást :D
---
+++ generate.php
@@ -0,0 +1,70 @@
+<?php
+
+if (count($argv) == 1) {
+	echo "Add a file as argument which contains the entity data.\n";
+	exit;
+}
+$data = [];
+$file = fopen($argv[1], "r") or die("Unable to open file!");
+do {
+	$line = fgets($file);
+	$data[] = $line;
+} while(!feof($file));
+fclose($file);
+
+$title = $data[0];
+$title = trim(preg_replace('/\s\s+/', '', $title));
+unset($data[0]);
+array_pop($data);
+$output = "";
+
+
+foreach ($data as $line) {
+	$var = explode(":", $line);
+	$var[1] = trim(preg_replace('/\s\s+/', '', $var[1]));
+	
+	if (count($var) > 1) {
+		$output .= comment($var[0], $var[1]);
+		$output .= "protected $" . $var[0] . ";\n\n";
+	}
+}
+
+$output .= "public static function baseFieldDefinitions(EntityTypeInterface \$entity_type) {\n";
+
+
+foreach ($data as $line) {
+	$var = explode(":", $line);
+	$var[1] = trim(preg_replace('/\s\s+/', '', $var[1]));
+	
+	if (count($var) > 1) {
+		$output .= "\t\$fields['" . $var[0] . "'] = BaseFieldDefinition::create('" . $var[1] . "')\n";
+		$output .= "\t\t->setLabel('" . $var[0] . "')\n";
+		$output .= "\t\t->setDescription(t('The " . ucfirst($var[0]) . " of the " . $title . " entity.'))\n";
+		$output .= "\t\t->setReadOnly(TRUE);\n\n";
+	}
+}
+
+$output .= "\treturn \$fields;\n}\n\n";
+
+foreach ($data as $line) {
+	$var = explode(":", $line);
+	$var[1] = trim(preg_replace('/\s\s+/', '', $var[1]));
+	
+	if (count($var) > 1) {
+		$output .= "/**\n *{@inheritdoc}\n */\n";
+		$output .= "public function get" . ucfirst($var[0]) . "() {\n";
+		$output .= "\treturn \$this->" . $var[0] . ";\n";
+		$output .= "}\n\n";
+	}
+}
+
+$outfile = fopen("output.txt", "w") or die("Unable to open file!");
+fwrite($outfile, $output);
+fclose($outfile);
+
+function comment($name, $type) {
+	global $title;
+	return "/**\n *  The " . $title . " " . $name . ".\n * \n *  @var " . $type . "\n */\n";
+}
+
+
